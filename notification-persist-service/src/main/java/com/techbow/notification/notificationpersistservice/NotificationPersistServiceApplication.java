2b0bc8c18741454f06225580a86633929eb85d96
yubo <lalagola@hotmail.com>
2021-05-30 01:58:13 -0400

    notification init

---
+++ notification-persist-service/src/main/java/com/techbow/notification/notificationpersistservice/NotificationPersistServiceApplication.java
@@ -0,0 +1,13 @@
+package com.techbow.notification.notificationpersistservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationPersistServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationPersistServiceApplication.class, args);
+	}
+
+}
---
+++ notification-persist-service/src/test/java/com/techbow/notification/notificationpersistservice/NotificationPersistServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techbow.notification.notificationpersistservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationPersistServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
